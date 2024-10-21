382b6041902a94508a7ee5e6a02942caae4b695b
keubung <skdud3390@naver.com>
2024-10-21 12:46:58 +0900

    [Add] Q1 -1, 2, 3

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    private static QuestManager instance;
+
+    public static QuestManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<QuestManager>();
+
+                if (instance == null)
+                {
+                    GameObject questManagerObject = new GameObject("QuestManager");
+                    instance = questManagerObject.AddComponent<QuestManager>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
